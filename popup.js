2a764d89147e903c1dd53fd0153f3543f35890a3
alaotach <aryanmi2001@gmail.com>
2026-04-08 10:28:28 +0530

    show groups in popup

--- popup.js
+++ popup.js
@@ -9,3 +9,20 @@ document.getElementById("createGroup").addEventListener("click", () => {
         alert("Group created!");
         location.reload();
     }
+});
+
+async function loadGroups() {
+    const d = await chrome.storage.local.get("groups");
+    const groups = d.groups || {};
+    const cunt = document.getElementById("groups");
+    cunt.innerHTML = '';
+    if (groups) {
+        Object.entries(groups).forEach(([id, group]) => {
+            const div = document.createElement("div");
+            div.textContent = group.name;
+            cunt.appendChild(div);
+        })
+    }
+}
+
+loadGroups();
