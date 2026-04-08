0694286d96f7403eaec94be48ee7e2c91084dc05
alaotach <aryanmi2001@gmail.com>
2026-04-08 12:13:35 +0530

    added custom alert box

--- popup.js
+++ popup.js
@@ -1,9 +1,25 @@
+const alert = document.createElement("div");
+alert.className = "alert";
+document.body.appendChild(alert);
+
+setTimeout(() => {
+    alert.textContent = "";
+}, 5000);
+
 document.getElementById("createGroup").addEventListener("click", async () => {
     const name = document.getElementById("groupName").value;
     if (name) {
         const d = await chrome.storage.local.get("groups");
         let groups = d.groups;
         if (!groups || Array.isArray(groups)) groups = {};
+        if (Object.values(groups).some(g => g.name === name)) {
+            alert.textContent = "A group with that name already exists.";
+            setInterval(() => {
+                alert.textContent = "";
+                alert.style.display = "none";
+            }, 5000);
+            return;
+        }
         const id = Date.now().toString();
         groups[id] = { name, tabs: [] };
         await chrome.storage.local.set({ groups });
@@ -28,7 +44,12 @@ async function loadGroups() {
       if (urls.length > 0) {
         chrome.windows.create({ url: urls });
       } else {
-        alert("No tabs in this group.");
+        alert.textContent = "No tabs in this group.";
+        setInterval(() => {
+                alert.textContent = "";
+                alert.style.display = "none";
+            }, 5000);
+            return;
       }
     };
 
